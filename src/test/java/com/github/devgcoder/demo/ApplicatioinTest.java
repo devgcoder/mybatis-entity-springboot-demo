c448656ad5e189a4f4715fc3b4cfae5b0e783eaf
duheng <dh@zhihuigongjiang.com>
2021-01-29 16:29:41 +0800

    增加单元测试

--- src/test/java/com/github/devgcoder/demo/ApplicatioinTest.java
+++ src/test/java/com/github/devgcoder/demo/ApplicatioinTest.java
@@ -2,6 +2,9 @@ package com.github.devgcoder.demo;
 
 import com.github.devgcoder.demo.user.entity.User;
 import com.github.devgcoder.mybatis.entity.mapper.MybatisEntityMapper;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,17 +19,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicatioinTest {
-
 	@Autowired
 	private MybatisEntityMapper mybatisEntityMapper;
-
 	@Test
-	public void insertEntity() {
-		User user = new User();
-		user.setName("duheng");
-		user.setAge(18);
-		Integer num = mybatisEntityMapper.insertEntity(user);
-		Long id = user.getId();
-		System.out.println("num=" + num + ",id=" + id);
+	public void testEntity() {
+		Map<String, Object> whereMap = new HashMap<>();
+		// name为表字段
+		whereMap.put("name", "王五2");
+		List<Map<String, Object>> list = mybatisEntityMapper.selectMapList(whereMap, User.class);
+		list.forEach(map -> {
+			System.out.println(map);
+		});
 	}
 }
