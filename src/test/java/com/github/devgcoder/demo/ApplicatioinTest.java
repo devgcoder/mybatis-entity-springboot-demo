bcf2e5df3bb5a548ebf90c3fc7a15e2d6b968d7b
duheng <dh@zhihuigongjiang.com>
2021-01-29 18:50:31 +0800

    新版本

--- src/test/java/com/github/devgcoder/demo/ApplicatioinTest.java
+++ src/test/java/com/github/devgcoder/demo/ApplicatioinTest.java
@@ -1,7 +1,8 @@
 package com.github.devgcoder.demo;
 
 import com.github.devgcoder.demo.user.entity.User;
-import com.github.devgcoder.mybatis.entity.mapper.MybatisEntityMapper;
+import com.github.devgcoder.mybatis.entity.service.MybatisEntityService;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,15 +21,39 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicatioinTest {
 	@Autowired
-	private MybatisEntityMapper mybatisEntityMapper;
+	private MybatisEntityService mybatisEntityService;
 	@Test
 	public void testEntity() {
-		Map<String, Object> whereMap = new HashMap<>();
-		// name为表字段
-		whereMap.put("name", "王五2");
-		List<Map<String, Object>> list = mybatisEntityMapper.selectMapList(whereMap, User.class);
-		list.forEach(map -> {
-			System.out.println(map);
+
+		User user = new User();
+		user.setName("张三");
+		user.setAge(18);
+		Integer num = mybatisEntityService.insertEntity(user);
+		Long id = user.getId();
+		System.out.println("num=" + num + ",id=" + id);
+		System.out.println("============");
+		List<User> userList = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			user = new User();
+			user.setName("王五" + i);
+			user.setAge(10 + i);
+			userList.add(user);
+		}
+		num = mybatisEntityService.insertEntityList(userList);
+		userList.forEach(u -> {
+			System.out.println("id=" + u.getId());
 		});
+		System.out.println("num=" + num);
+		System.out.println("============");
+		Map<String, Object> whereMap = new HashMap<>();
+		whereMap.put("name", "王五1");
+		userList = mybatisEntityService.selectEntityList(whereMap, User.class);
+		System.out.println();
+		user = mybatisEntityService.selectOneEntity(11, User.class);
+		System.out.println();
+		List<Map<String, Object>> mapList = mybatisEntityService.selectMapList(whereMap, User.class);
+		System.out.println();
+		Map<String, Object> map = mybatisEntityService.selectOneMap(12, User.class);
+		System.out.println();
 	}
 }
